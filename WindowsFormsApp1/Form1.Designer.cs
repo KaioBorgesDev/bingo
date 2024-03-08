727f9f4b954b30fe630fb4ee75520180b0c99faa
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2024-03-08 17:18:25 -0300

    logica de bingo usuario

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -44,7 +45,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label1.Location = new System.Drawing.Point(315, 9);
+            this.label1.Location = new System.Drawing.Point(298, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(246, 55);
             this.label1.TabIndex = 0;
@@ -54,16 +55,18 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(587, 57);
+            this.label2.Location = new System.Drawing.Point(601, 57);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(175, 33);
             this.label2.TabIndex = 1;
             this.label2.Text = "Computador";
+            this.label2.Visible = false;
             // 
             // button1
             // 
+            this.button1.Enabled = false;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(348, 160);
+            this.button1.Location = new System.Drawing.Point(410, 473);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -126,17 +129,29 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label8.Location = new System.Drawing.Point(351, 89);
+            this.label8.Location = new System.Drawing.Point(400, 87);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(134, 55);
+            this.label8.Size = new System.Drawing.Size(51, 55);
             this.label8.TabIndex = 8;
-            this.label8.Text = "NUM";
+            this.label8.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 473);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "COMEÇAR UM JOGO";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(871, 508);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -164,6 +179,7 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,24 +12,23 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+
         private Button[,] bingoButtons = new Button[5, 5];
         private Button[,] bingoButtonsComputer = new Button[5, 5];
         private Random random = new Random();
+        private Timer timer = new Timer();
+        HashSet<int> numbers = new HashSet<int>();
         public Form1()
         {
             InitializeComponent();
-            InitializeBingoBoard();
-            InitializeBingoBoardComputer();
+            
         }
 
         private void Panel1_Paint(object sender, PaintEventArgs e)
         {
 
         }
-        private void InitializeNumbersGenerated()
-        {
-           
-        }
+        
         private void InitializeBingoBoard()
         {
             const int buttonSize = 50;
@@ -40,6 +39,7 @@ namespace WindowsFormsApp1
             {
                 for (int col = 0; col < 5; col++)
                 {
+                    
                     Button button = new Button();
                     button.SetBounds(10, 10, 10, 10);
                     button.Width = button.Height = buttonSize;
@@ -47,6 +47,10 @@ namespace WindowsFormsApp1
                     button.Left = col * (buttonSize + padding);
                     button.Tag = false; // Tag to keep track of whether the number is marked or not
                     button.Click += BingoButton_Click;
+                    if (row == 2 && col == 2)
+                    {
+                        button.Enabled = false;
+                    }
                     bingoButtons[row, col] = button;
                     Controls.Add(button);
                 }
@@ -58,7 +62,7 @@ namespace WindowsFormsApp1
             const int buttonSize = 50;
             const int padding = 5;
             const int positionY = 100;
-            const int positionX = 500;
+            const int positionX = 590;
 
             for (int row = 0; row < 5; row++)
             {
@@ -78,66 +82,186 @@ namespace WindowsFormsApp1
             }
             GenerateRandomNumbers(bingoButtonsComputer);
         }
+        private void GenerateRandomNumber()
+        {
+            int randomNumber = random.Next(1, 76); // Número aleatório de 1 a 75
+
+            while (numbers.Contains(randomNumber))
+            {
+                randomNumber = random.Next(1, 76); // Gera um novo número aleatório
+            }
+
+            numbers.Add(randomNumber);
+            label8.Text = randomNumber.ToString();
+
+        }
 
         private void GenerateRandomNumbers(Button[,] bingoButtons)
         {
-            int[] numbers = new int[75];
-            for (int i = 0; i < 75; i++)
-            {
-                numbers[i] = i + 1;
-            }
+            // Intervalos para cada coluna
+            int[] intervals = { 15, 30, 45, 60, 75 };
+            int start = 1; // Valor inicial para a primeira coluna
 
-            for (int row = 0; row < 5; row++)
+            for (int col = 0; col < 5; col++)
             {
-                for (int col = 0; col < 5; col++)
+                int end = intervals[col]; // Valor final para a coluna atual
+
+                List<int> numbers = new List<int>();
+
+                // Preenche a lista de números no intervalo correspondente
+                for (int i = start; i <= end; i++)
                 {
-                    int index = random.Next(0, 75);
-                    int temp = numbers[index];
-                    numbers[index] = numbers[row * 5 + col];
-                    numbers[row * 5 + col] = temp;
+                    numbers.Add(i);
                 }
-            }
 
-            for (int row = 0; row < 5; row++)
-            {
-                for (int col = 0; col < 5; col++)
+                // Embaralha os números na lista
+                for (int i = 0; i < numbers.Count; i++)
                 {
-                    bingoButtons[row, col].Text = numbers[row * 5 + col].ToString();
-
+                    int temp = numbers[i];
+                    int randomIndex = random.Next(i, numbers.Count);
+                    numbers[i] = numbers[randomIndex];
+                    numbers[randomIndex] = temp;
                 }
+
+                // Preenche os botões com os números aleatórios
+                for (int row = 0; row < 5; row++)
+                {
+                    bingoButtons[row, col].Text = numbers[row].ToString();
+                }
+
+                start = end + 1; // Atualiza o valor inicial para a próxima coluna
             }
         }
 
         private void BingoButton_Click(object sender, EventArgs e)
         {
             Button clickedButton = (Button)sender;
-            bool marked = (bool)clickedButton.Tag;
-            clickedButton.Tag = !marked;
-            clickedButton.BackColor = marked ? SystemColors.Control : SystemColors.Highlight;
-            clickedButton.Enabled = false;
+            string valorButton = clickedButton.Text;
+            string numeroSort = label8.Text;
+            if (valorButton == numeroSort)
+            {
+                bool marked = (bool)clickedButton.Tag;
+                clickedButton.Tag = !marked;
+                clickedButton.BackColor = marked ? SystemColors.Control : SystemColors.Highlight;
+                clickedButton.Enabled = false;
+            }
+            
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-           bool ganhou = verificarFim();
-            if (ganhou)
+           bool ganhou = verificarBingo();
+            if (!ganhou)
             {
-                MessageBox.Show("PARA DE GRITAR EM FALSO");
+                MessageBox.Show("Voce ta mentindo!","Mentiroso!!!PARA DE GRITAR EM FALSO");
             }
             else
             {
-                MessageBox.Show("PARABENS VOCE GANHOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+                MessageBox.Show("PARABENS VOCE GANHOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU","Voce foi sortudo!");
             }
         }
-
-        private bool verificarFim()
+        private bool VerificarLinhaHabilitada(int linha)
         {
-            
-            if (bingoButtons[0,0].Enabled)
+            for (int j = 0; j < 5; j++)
             {
-                return true;
+                if (bingoButtons[linha, j].Enabled)
+                {
+                    // Se encontrar um botão desabilitado, a linha não está completamente habilitada
+                    return false;
+                }
             }
+
+            
+            return true;
+        }
+        
+        private bool verificarColunaHabilitada(int coluna)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (bingoButtons[i, coluna].Enabled)
+                {
+                   
+                    return false;
+                }
+            }
+
+            
+            return true;
+        }
+
+        private bool verificarDiagonalPrincipalHabilitada()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (bingoButtons[i, i].Enabled)
+                {
+                    // Se encontrar um botão desabilitado, a diagonal principal não está completamente habilitada
+                    return false;
+                }
+            }
+
+            // Se todos os botões da diagonal principal estiverem habilitados, retorna verdadeiro
+            return true;
+        }
+
+        private bool verificarDiagonalSecundariaHabilitada()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                if (bingoButtons[i, 4 - i].Enabled)
+                {
+                    // Se encontrar um botão desabilitado, a diagonal secundária não está completamente habilitada
+                    return false;
+                }
+            }
+
+            // Se todos os botões da diagonal secundária estiverem habilitados, retorna verdadeiro
+            return true;
+        }
+
+        private bool verificarBingo()
+        {
+            // Verifica se alguma linha ou coluna está habilitada (todos os botões estão habilitados)
+            for (int i = 0; i < 5; i++)
+            {
+                if (VerificarLinhaHabilitada(i) || verificarColunaHabilitada(i) || verificarDiagonalPrincipalHabilitada() 
+                    || verificarDiagonalSecundariaHabilitada())
+                {
+                    // Se encontrar uma linha ou coluna completamente habilitada, retorna true (bingo)
+                    return true;
+                }
+            }
+
+            // Se nenhuma linha ou coluna estiver completamente habilitada, retorna false
             return false;
+        }
+
+        private void InitializeTimer()
+        {
+            timer.Interval = 5000; // Intervalo em milissegundos (5 segundos)
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+            
+        {
+            GenerateRandomNumber();
+        }
+
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //jogo começado
+            InitializeTimer();
+            InitializeBingoBoard();
+            InitializeBingoBoardComputer();
+            
+            GenerateRandomNumber();
+            button1.Enabled = true;
+            label2.Visible = true;
+            button2.Enabled = false;
         }
     }
 }
