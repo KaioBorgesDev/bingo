95cec186bdc185f9d0f2fce4ef1df39c7f4bb1c7
aluno <aluno@fatec.campinas.br>
2024-03-08 12:44:04 -0300

    First

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(315, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(246, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CARTELA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(587, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Computador";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(348, 160);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "BINGO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(-1, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 55);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(61, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 55);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "I";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(104, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 55);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "N ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(165, 35);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 55);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "G";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(221, 35);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 55);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "O";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label8.Location = new System.Drawing.Point(351, 89);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(134, 55);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "NUM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Button[,] bingoButtons = new Button[5, 5];
+        private Button[,] bingoButtonsComputer = new Button[5, 5];
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeBingoBoard();
+            InitializeBingoBoardComputer();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+        private void InitializeNumbersGenerated()
+        {
+           
+        }
+        private void InitializeBingoBoard()
+        {
+            const int buttonSize = 50;
+            const int padding = 5;
+            const int positionY = 100;
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    Button button = new Button();
+                    button.SetBounds(10, 10, 10, 10);
+                    button.Width = button.Height = buttonSize;
+                    button.Top = positionY + row * (buttonSize + padding);
+                    button.Left = col * (buttonSize + padding);
+                    button.Tag = false; // Tag to keep track of whether the number is marked or not
+                    button.Click += BingoButton_Click;
+                    bingoButtons[row, col] = button;
+                    Controls.Add(button);
+                }
+            }
+            GenerateRandomNumbers(bingoButtons);
+        }
+        private void InitializeBingoBoardComputer()
+        {
+            const int buttonSize = 50;
+            const int padding = 5;
+            const int positionY = 100;
+            const int positionX = 500;
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    Button button = new Button();
+                    button.SetBounds(10, 10, 10, 10);
+                    button.Width = button.Height = buttonSize;
+                    button.Top = positionY + row * (buttonSize + padding);
+                    button.Left = positionX + col * (buttonSize + padding);
+                    button.Tag = false; // Tag to keep track of whether the number is marked or not
+                    button.Enabled = false;
+                    button.Click += BingoButton_Click;
+                    bingoButtonsComputer[row, col] = button;
+                    Controls.Add(button);
+                }
+            }
+            GenerateRandomNumbers(bingoButtonsComputer);
+        }
+
+        private void GenerateRandomNumbers(Button[,] bingoButtons)
+        {
+            int[] numbers = new int[75];
+            for (int i = 0; i < 75; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    int index = random.Next(0, 75);
+                    int temp = numbers[index];
+                    numbers[index] = numbers[row * 5 + col];
+                    numbers[row * 5 + col] = temp;
+                }
+            }
+
+            for (int row = 0; row < 5; row++)
+            {
+                for (int col = 0; col < 5; col++)
+                {
+                    bingoButtons[row, col].Text = numbers[row * 5 + col].ToString();
+
+                }
+            }
+        }
+
+        private void BingoButton_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            bool marked = (bool)clickedButton.Tag;
+            clickedButton.Tag = !marked;
+            clickedButton.BackColor = marked ? SystemColors.Control : SystemColors.Highlight;
+            clickedButton.Enabled = false;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+           bool ganhou = verificarFim();
+            if (ganhou)
+            {
+                MessageBox.Show("PARA DE GRITAR EM FALSO");
+            }
+            else
+            {
+                MessageBox.Show("PARABENS VOCE GANHOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU");
+            }
+        }
+
+        private bool verificarFim()
+        {
+            
+            if (bingoButtons[0,0].Enabled)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
